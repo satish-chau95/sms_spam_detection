bf3540240cde73970d9fe98526367473bf32ef43
Satish <harekrishna80108@gmail.com>
2025-05-08 01:43:45 +0530

    new commit

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, render_template
+import pickle
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+app = Flask(__name__)
+
+ps = PorterStemmer()
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+# Load model and vectorizer
+model = pickle.load(open('model.pkl', 'rb'))
+vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    prediction = None
+    if request.method == 'POST':
+        input_sms = request.form.get('message')
+        transformed_sms = transform_text(input_sms)
+        vector_input = vectorizer.transform([transformed_sms])
+        result = model.predict(vector_input)[0]
+        prediction = "Spam" if result == 1 else "Not Spam"
+    return render_template('index.html', prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
