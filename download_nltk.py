938a3837e55cecc8c7d8dc568e756a9938aa88d3
Satish <harekrishna80108@gmail.com>
2025-05-08 11:28:45 +0530

    new commit

---
+++ download_nltk.py
@@ -0,0 +1,5 @@
+import nltk
+
+# Download required NLTK datasets to a local folder named 'nltk_data'
+nltk.download('punkt', download_dir='nltk_data')
+nltk.download('stopwords', download_dir='nltk_data')
